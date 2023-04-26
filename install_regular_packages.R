35a7d04a2e88947600842d90ba4a1a369c2416df
Prateek Arora <15521391+pratarora@users.noreply.github.com>
2023-04-26 10:57:47 +0200

    updated the packages required

--- install_regular_packages.R
+++ install_regular_packages.R
@@ -1,28 +1,36 @@
+list_of_packages <- c("ggplot2", "dplyr", "gridGraphics", "gridExtra", 
+                      "ggrepel", "DBI", "svglite", "htmltools", "org.Mm.eg.db", 
+                      "igraph", "psych", "org.Dr.eg.db", "clusterProfiler", 
+                      "edgeR", "UpSetR", "plyr", "DESeq2", "readxl", "plotly", 
+                      "data.table", "installr", "knitr", "gtools", "vsn", 
+                      "SummarizedExperiment", "sva", "BiocParallel", "biomaRt", 
+                      "curl", "stats", "rvcheck", "tidyverse", "grid", "brew",
+                      "VennDiagram", "RColorBrewer", "ComplexHeatmap", 
+                      "colorspace", "circlize", "tidyr", "matrixStats", "rlang",
+                      "sodium", "devtools", "shinymanager", "DT", "fontawesome",
+                      "shinydashboard", "shinycssloaders", "rmarkdown", 
+                      "markdown", "visNetwork", "heatmaply","wesanderson", 
+                      "shinyauthr", "Seurat", "shinythemes","ggVennDiagram",
+                      "caret", "tidymodels", "BiocManager", "languageserver", 
+                      "patchwork", "ggbiplot", "DiffBind", "GenomicFeatures", 
+                      "ChIPseeker", "rstatix", "ggpubr", "viridis", 
+                      "ReactomePA", "Cardinal", "MetaboAnnotation", "IRkernel", 
+                      "RforMassSpectrometry/RforMassSpectrometry")
 
-# List of packages to check and install
-packages <- c("tidyverse","ggplot2", "dplyr", "gridGraphics", "gridExtra", "ggrepel", "DBI", "svglite", "htmltools", "biomaRt","BiocManager",
-              "org.Mm.eg.db", "igraph", "psych", "org.Dr.eg.db", "clusterProfiler", "ggplot2", "gridExtra",
-              "edgeR", "UpSetR", "plyr", "DESeq2", "readxl", "plotly", "data.table", "installr", "knitr",
-              "gtools", "vsn", "SummarizedExperiment", "gridExtra", "sva", "BiocParallel", "biomaRt", "curl",
-              "stats", "rvcheck", "tidyverse", "grid", "VennDiagram", "RColorBrewer", "ComplexHeatmap",
-              "svglite", "colorspace", "circlize", "tidyr", "rlang", "sodium", "devtools", "shinymanager",
-              "DT", "fontawesome", "shinydashboard", "shinycssloaders", "igraph", "rmarkdown", "markdown",
-              "visNetwork", "heatmaply", "plotly", "brew", "RColorBrewer", "curl", "wesanderson", "shinyauthr",
-              "Seurat",  "Seurat", "patchwork", "sva", "ggbiplot", "edgeR", "DiffBind",
-              "GenomicFeatures", "ChIPseeker", "rstatix", "ggpubr", "viridis", "ReactomePA", "clusterProfiler",
-              "org.Mm.eg.db", "Cardinal", "RforMassSpectrometry/RforMassSpectrometry", "MetaboAnnotation", "IRkernel", "languageserver")
 
-new_pkgs <- packages[!packages %in% installed.packages()]
-new_pkgs <- unique(new_pkgs)
-new_pkgs
-install.packages(new_pkgs)
 
-new_pkgs <- packages[!packages %in% installed.packages()]
-new_pkgs <- unique(new_pkgs)
-new_pkgs
-BiocManager::install(new_pkgs)
+new_packages <- list.of.packages[!(list_of_packages %in% installed.packages()[,"Package"])]
+new_packages <- unique(new_packages)
+new_packages
+if(length(new_packages)) install.packages(new_packages)
 
-ainstall.packages("devtools")
+
+
+
+new_packages <- list.of.packages[!(list_of_packages %in% installed.packages()[,"Package"])]
+new_packages <- unique(new_packages)
+new_packages
+BiocManager::install(new_packages)
+
 devtools::install_github("neurorestore/Libra")
-
-
+devtools::install_github("SGDDNB/ShinyCell")
